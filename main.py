79409e64edface244aae6343558d0c1a753adcec
Richard Olpindo <djricky088@yahoo.com>
2026-05-05 12:41:39 -1000

    Corrected first challenge

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ screen.title("My Snake Game")
 root = screen.getcanvas().winfo_toplevel()
 root.call('wm', 'attributes', '.', '-topmost', '1')
 
-snake_length = 3
+# snake_length = 3
+#
+# for i in range(snake_length):
+#     sp = Turtle()
+#     sp.color("white")
+#     sp.shape("square")
+#     sp.setx(i*-20)
 
-for i in range(snake_length):
-    sp = Turtle()
-    sp.color("white")
-    sp.shape("square")
-    sp.setx(i*-20)
+starting_positions = [(0,0), (-20,0), (-40,0)]
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 screen.exitonclick()
