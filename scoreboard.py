20049dab8149a26c872cd61a98c32a4418669403
Richard Olpindo <djricky088@yahoo.com>
2026-05-06 12:49:12 -1000

    Updated score update functions, changed font preferences to constants

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 
+ALIGNMENT = 'center'
+FONT = ('Courier', 30 , "normal")
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -8,11 +11,13 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(x=0, y=265)
         self.hideturtle()
+        self.update_score()
 
     def update_score(self):
-        self.clear()
-        self.write(arg=f"Score: {self.score}",move=False, align="center", font= ('Courier', 30, "normal"))
+        self.write(arg=f"Score: {self.score}",move=False, align=ALIGNMENT, font= FONT)
 
     def add_point(self):
         self.score += 1
+        self.clear()
+        self.update_score()
 
