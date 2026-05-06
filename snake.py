38f0100163e97268ea7847c083fbecaf8ebd2221
Richard Olpindo <djricky088@yahoo.com>
2026-05-05 22:35:22 -1000

    Create conditionals that prevent the snake from moving backwards on itself

--- snake.py
+++ snake.py
@@ -2,6 +2,10 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)] #Capitalized means that this is a constant
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
@@ -22,16 +26,20 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
