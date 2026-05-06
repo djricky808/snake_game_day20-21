af190b26ebb80e9297c574652c6fa1c6310b714c
Richard Olpindo <djricky088@yahoo.com>
2026-05-05 22:11:14 -1000

    Replaced the hard coded snake code with the imported snake class

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ screen.tracer(0)
 root = screen.getcanvas().winfo_toplevel()
 root.call('wm', 'attributes', '.', '-topmost', '1')
 
+snake = Snake()
+
 game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
+    snake.move()
 
 screen.exitonclick()
